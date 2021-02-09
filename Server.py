77fdd19036bdac22a014da7500d860d9c907fb71
Anonymous_Programmer <68592417+SATAN01@users.noreply.github.com>
2021-02-09 16:56:42 +0530

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,47 @@
+import socket, threading
+host = '127.0.0.1'
+port = 65432
+clients = []
+names = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen()
+
+
+def sendall(msg): 
+    for client in clients:
+        client.send(msg)
+
+
+def recv_broadcast(client):
+    while True:
+        try:
+            msg1 = client.recv(1024)
+            sendall(msg1)
+        except:
+            cd_in = clients.index(client)
+            clients.pop(cd_in)
+            client.close()
+            print(f"{names[cd_in]} left the chat!")
+            sendall(f"{names[cd_in]} left the chat!".encode())
+            names.pop(cd_in)
+            break
+
+
+def main():
+    print("Server is listening on port {} and ip {}".format(port,host))
+    while True:
+        conn,addr = s.accept()
+        print("connected by ", addr)
+        conn.send("usr".encode())
+        client_name = conn.recv(1024)
+        names.append(client_name)
+        clients.append(conn)
+        print(client_name)
+        sendall(f"{client_name} joined the chat! \n".encode())
+        conn.send("You joined the chat! \n".encode())
+        thread = threading.Thread(target=recv_broadcast,args=(conn,))
+        thread.start()
+
+
+main()
